4a65eab854d2430b3718c42f8d4388d7c0ed74ac
Ryan Amburgy <ryan.amburgy@gmail.com>
2013-01-13 15:55:10 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gameswap::Application.config.secret_token = '0768f841a81366d2bd6c58f21de8fe4f56c33599c4084ef25b5367c6cfdcf5fd36e0efca3506e41920b9b9fe1edc5f6837954f1aa513440b1321e73ddadb4ad3'
